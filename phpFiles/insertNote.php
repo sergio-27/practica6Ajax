fb89528d0adc67e74c531cc46dbb0738e30f7618
sergi <sergio96ruiz@gmail.com>
2018-03-01 01:42:57 +0100

    test

---
+++ phpFiles/insertNote.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$database = 'practicanotas';
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $database) or die("Error");
+
+
+//obtenemos los valores del notecontent
+$noteContent = $_REQUEST['noteContent'];
+
+//consulta para insertar nota
+$insertNoteQuery = 'INSERT INTO notas (noteid, noteContent, userid, noteTitle) VALUES (3, '.$noteContent.', 1, hello)';
+
+//ejecutamos la consulta
+//mysqli_query($conn, $insertNoteQuery);
+
+
+if (mysqli_query($conn, $insertNoteQuery)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $insertNoteQuery . "<br>" . mysqli_error($conn);
+}
+
+$resposta2 = '{noteContent: '.$noteContent.'}';
+
+if (isset($_GET['callback'])) {
+    echo $_GET['callback'] . '(' . $resposta2 . ')';
+} else {
+    echo ($resposta2);
+} 
